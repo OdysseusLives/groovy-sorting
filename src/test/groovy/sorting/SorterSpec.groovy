f3b15102dc4bc86b7c69c7e470d74caa39d9aa59
Aubrey Chipman <aubrey.chipman@gmail.com>
2015-12-29 17:43:50 -0600

    Update parameterized testing have parameter re-use shortly

--- src/test/groovy/sorting/SorterSpec.groovy
+++ src/test/groovy/sorting/SorterSpec.groovy
@@ -1,23 +1,35 @@
 package sorting
 
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
 
 class SorterSpec extends Specification {
-    @Unroll("Bubble sort: #testCase")
-    def "bubble sort can order a list"(List unsorted, List sorted, String testCase) {
+    @Shared
+    def parameterSet = [
+        [testCase: "should order A & B when A is first",
+            unsorted: ["A", "B"], sorted: ["A", "B"]],
+        [testCase: "should order A & B when B is first",
+            unsorted: ["B", "A"], sorted: ["A", "B"]],
+        [testCase: "should order numbers",
+            unsorted: [2.0, 1], sorted: [1, 2.0]],
+        [testCase:  "should order with a deep investigation",
+            unsorted: ["Cat", "Car"], sorted: ["Car", "Cat"]],
+        [testCase:  "should order capitals before lowercase",
+            unsorted: ["Cat", "car"], sorted: ["Cat", "car"]],
+        [testCase:  "should order more than two items",
+            unsorted: ["B", "A", "D", "C"], sorted: ["A", "B", "C", "D"]],
+        [testCase:  "should order using multiple passes",
+            unsorted: ["E", "B", "D", "A", "C"], sorted: ["A", "B", "C", "D", "E"]]
+    ]
+
+    @Unroll("Bubble sort: #params.testCase")
+    def "bubble sort can order a list"() {
         expect:
-        Sorter.bubbleSort(unsorted) == sorted
+        Sorter.bubbleSort(params.unsorted) == params.sorted
 
         where:
-        unsorted | sorted | testCase
-        ["A", "B"] | ["A", "B"] | "should order A & B when A is first"
-        ["B", "A"] | ["A", "B"] | "should order A & B when B is first"
-        [2.0, 1] | [1, 2.0] | "should order numbers"
-        ["Cat", "Car"] | ["Car", "Cat"] | "should order with a deep investigation"
-        ["Cat", "car"] | ["Cat", "car"] | "should order capitals before lowercase"
-        ["B", "A", "D", "C"] | ["A", "B", "C", "D"] | "should order more than two items"
-        ["E", "B", "D", "A", "C"] | ["A", "B", "C", "D", "E"] | "should order using multiple passes"
+        params << parameterSet
     }
 
     @Unroll("Insertion sort: #testCase")
@@ -29,6 +41,11 @@ class SorterSpec extends Specification {
         unsorted | sorted | testCase
         ["A", "B"] | ["A", "B"] | "should order A & B when A is first"
         ["B", "A"] | ["A", "B"] | "should order A & B when B is first"
+        [2.0, 1] | [1, 2.0] | "should order numbers"
+        ["Cat", "Car"] | ["Car", "Cat"] | "should order with a deep investigation"
+        ["Cat", "car"] | ["Cat", "car"] | "should order capitals before lowercase"
+//        ["B", "A", "D", "C"] | ["A", "B", "C", "D"] | "should order more than two items"
+//        ["E", "B", "D", "A", "C"] | ["A", "B", "C", "D", "E"] | "should order using multiple passes"
     }
 
 }
