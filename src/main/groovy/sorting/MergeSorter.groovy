95d5cfcb1aae7a32d0b4a28bef790eedfbe39010
Aubrey Chipman <aubrey.chipman@gmail.com>
2016-01-01 14:24:43 -0600

    Merge sort - sorts groups of two

--- src/main/groovy/sorting/MergeSorter.groovy
+++ src/main/groovy/sorting/MergeSorter.groovy
@@ -2,11 +2,23 @@ package sorting
 
 class MergeSorter extends Sorter {
     static List sort(List unsorted) {
-        def individuals = unsorted.collate(unsorted.size() - 1)
+        def individuals = unsorted.collate(1)
 
-        def sorted = mergeTwoElements(individuals[0][0], individuals[1][0])
+        def sorted = new ArrayList()
+
+        for(def i = 0; i < individuals.size(); i = i + 2) {
+            sorted = sorted.plus(mergeTwoElementsAndHandleRemainders(individuals, i))
+        }
 
         sorted
+    }
+
+    private static ArrayList<Object> mergeTwoElementsAndHandleRemainders(List<List> individuals, int i) {
+        if(i + 1 < individuals.size()) {
+            mergeTwoElements(individuals[i][0], individuals[i + 1][0])
+        } else {
+            individuals[i]
+        }
     }
 
     static List mergeTwoElements(Object first, Object second) {
