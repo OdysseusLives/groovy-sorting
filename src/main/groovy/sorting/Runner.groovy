a14ef716183a2db78be225b38b9785a7d9aa4131
Aubrey Chipman <aubrey.chipman@gmail.com>
2016-01-01 16:24:35 -0600

    Runner - compare speed of various sorting algorithms

--- src/main/groovy/sorting/Runner.groovy
+++ src/main/groovy/sorting/Runner.groovy
@@ -1,3 +1,26 @@
 package sorting
 
-println 'Groovy world!'
+import org.apache.commons.lang3.time.StopWatch
+
+println "Sorting algorithms time comparison\n"
+
+println "Times listed in the following format: hours:minutes:seconds.milliseconds\n"
+
+List list = new ArrayList(1..10_000)
+Collections.shuffle(list)
+
+recordTime("Bubble sort", list, {BubbleSorter.sort(listClone)})
+recordTime("Insertion sort", list, {InsertionSorter.sort(listClone)})
+recordTime("Groovy built-in sort", list, {listClone.sort()})
+
+private void recordTime(String title, List list, Closure closure) {
+    StopWatch stopWatch = new StopWatch()
+    stopWatch.start()
+    for (def i = 0; i < 10_000; i++) {
+        def listClone = list.clone() as List
+        closure
+    }
+    stopWatch.stop()
+
+    println " - " + title + ": " + stopWatch.toString()
+}
