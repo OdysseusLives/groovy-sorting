227d473786f2711e28fa1b8f6a0c91b9cfdf1e2f
Aubrey Chipman <aubrey.chipman@gmail.com>
2016-01-01 15:21:54 -0600

    Groovy's built-in sort has all the features I'm testing for.  It'll be a good benchmark

--- src/test/groovy/sorting/SorterSpec.groovy
+++ src/test/groovy/sorting/SorterSpec.groovy
@@ -63,4 +63,24 @@ class SorterSpec extends Specification {
         [] | [] | "should order a list of zero"
 //        ["a", "c", "b", "b"] | ["a", "b", "b", "c"] | "should preserve duplicates"
     }
+
+    @Unroll("Groovy built-in sort: #testCase")
+    def "groovy built-in sort can order a list"(List unsorted, List sorted, String testCase) {
+        expect:
+        unsorted.sort() == sorted
+
+        where:
+        unsorted | sorted | testCase
+        ["A", "B"] | ["A", "B"] | "should preserve order when already ordered"
+        ["B", "A"] | ["A", "B"] | "should order letters"
+        [2.0, 1] | [1, 2.0] | "should order numbers"
+        ["Cat", "Car"] | ["Car", "Cat"] | "should order with a deep investigation"
+        ["Cat", "car"] | ["Cat", "car"] | "should order capitals before lowercase"
+        ["B", "A", "C"] | ["A", "B", "C"] | "should order an odd number of items"
+        ["B", "A", "D", "C"] | ["A", "B", "C", "D"] | "should order more than two items, when subgroups are in order"
+        ["E", "B", "D", "A", "C"] | ["A", "B", "C", "D", "E"] | "should order using multiple passes"
+        ["a"] | ["a"] | "should order a list of one"
+        [] | [] | "should order a list of zero"
+        ["a", "c", "b", "b"] | ["a", "b", "b", "c"] | "should preserve duplicates"
+    }
 }
